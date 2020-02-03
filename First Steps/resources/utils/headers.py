83a5fb68bc3aeaa42d44f9e3c1d30cf232de062f
CarinaCruz <carinacruzteixeira@gmail.com>
2020-02-03 19:14:45 -0300

    First steps with PyTorch

---
+++ First Steps/resources/utils/headers.py
@@ -0,0 +1,20 @@
+# Module I
+
+import torch 
+import matplotlib.pylab as plt
+import numpy as np 
+import pandas as pd
+from torch.utils.data import Dataset, DataLoader
+torch.manual_seed(0)
+
+from matplotlib.pyplot import imshow
+import matplotlib.pylab as plt
+from PIL import Image
+import os
+
+import torchvision.transforms as transforms
+import torchvision.datasets as dsets
+
+# Module II
+from torch.nn import Linear
+from torch import nn
